f5ca0311741ccf80c673a6eba54575da69d2b847
girija00330-design <girija00330@gmail.com>
2026-01-03 19:38:52 +0530

    Implement addBook function for book list
    Added a function to add a book to the list with validation.

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function showMessage() {
-  alert("GitHub Pages Deployment Successful!");
+function addBook() {
+  let book = document.getElementById("bookName").value;
+  let author = document.getElementById("author").value;
+
+  if (book === "" || author === "") {
+    alert("Please fill all fields");
+    return;
+  }
+
+  let li = document.createElement("li");
+  li.innerText = book + " by " + author;
+
+  document.getElementById("bookList").appendChild(li);
+
+  document.getElementById("bookName").value = "";
+  document.getElementById("author").value = "";
 }
